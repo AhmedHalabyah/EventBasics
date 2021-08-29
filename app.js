3dfe1840b4db22498cb6789bb0845704bc9a8361
AhmedHalabyah <ahmedhalabyah96@gmail.com>
2021-08-29 18:53:26 +0300

    Score

--- app.js
+++ app.js
@@ -1,42 +1,74 @@
-const btn = document.querySelector('#v2');
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
+const p1Display = document.querySelector('#p1Score');
+const p2Display = document.querySelector('#p2Score');
+const winningScoreSelect = document.querySelector('#playto');
 
- btn.onclick = function() {
-     console.log("YOU CLICKED ME !")
-     console.log("I HOPE IT WORKED!!")
- }
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 3;
+let isGameOver = false;
 
 
-function scream(){
-    console.log("AAAAHHHHHHH");
-    console.log("STOP TOUCHING ME!")
+p1Button.addEventListener('click', function () {
+    if(!isGameOver) {
+        p1Score += 1;
+        if(p1Score === winningScore){
+            isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+})
+
+p2Button.addEventListener('click', function () {
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+
+        }
+         p2Display.textContent = p2Score;
+    }
+    
+})
+
+winningScoreSelect.addEventListener('change', function () {
+  winningScore = parseInt(this.value);
+  reset();
+
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
 
-
- btn.onmouseenter = scream;
-
-
- document.querySelector('h1').onclick = () => {
-     alert('you clicked the h1!')
- }
+  
 
 
- const btn3 = document.querySelector('#v3')
- btn3.addEventListener('click', function(){
-     alert("CLICKED!")
- })
-
- function twist() {
-     console.log("TWIST!")
- }
-
- function shout() {
-    console.log("SHOUT!")
-}
-
-const tasButton = document.querySelector('#tas')
-
-//   tasButton.onclick = twist;
-//   tasButton.onclick = shout; 
-
-tasButton.addEventListener('click', twist)
-tasButton.addEventListener('click', shout)
+   
+    // for(let p of [p1, p2]) {
+    //     p.score = 0,
+    //     p.p1display.textContent = 0,
+    //     p.display.classList.remove('has-text-success', 'has-text-danger');
+    //     p.button.disabled = false;
+    // }
